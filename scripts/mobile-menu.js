c4f602cefdf5f9a2c8a136c79c7b8507bb160549
wavinginspace <paulbaisley@gmail.com>
2020-01-25 12:57:19 -0500

    x icon + link color

--- scripts/mobile-menu.js
+++ scripts/mobile-menu.js
@@ -1,5 +1,7 @@
-$('nav').on('click', '.hamburger-button', function() {
+$('nav').on('click', '.menu-button', function() {
   $('.mobile-menu').toggleClass('open');
   $('#burger-svg').toggleClass('burger-closed');
   $('#burger-svg').toggleClass('burger-open');
+  $('#x-icon').toggleClass('x-icon-closed');
+  $('#x-icon').toggleClass('x-icon-open');
 });
